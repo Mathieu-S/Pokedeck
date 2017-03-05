2452d485d56a9fbdc34173806973b7cedd00a4a8
allanbz <allan.bezerra017@gmail.com>
2017-03-04 23:18:13 -0300

    Pull request
    Added a way to show name of the cards on the screen.

--- src/main/java/com/pokedeck/Window.java
+++ src/main/java/com/pokedeck/Window.java
@@ -1,24 +1,26 @@
 package com.pokedeck;
 
+import com.pokedeck.cards.*;
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class Window extends JFrame{
 
-    public Window(){
+    public Window(ArrayList<Card> cards){
         this.setTitle("Pokedeck");
         this.setSize(650, 400);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setVisible(true);
-
+        
         DefaultListModel listModel = new DefaultListModel();
-        listModel.addElement("Jane Doe");
-        listModel.addElement("John Smith");
-        listModel.addElement("Kathy Green");
-
+        
+        for (Card c : cards) {
+        	listModel.addElement(c.getCardName());
+        }
+        
         JComponent list = new JList(listModel);
-
+        
         this.add(list);
-
     }
 }
