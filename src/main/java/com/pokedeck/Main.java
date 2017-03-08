2872058afc11bb057f673bdd06eb8c9fbb15ee79
Mathieu-S <mathieusartorio@gmail.com>
2017-03-08 22:15:12 +0100

    Ajout / supression carte de la liste

--- src/main/java/com/pokedeck/Main.java
+++ src/main/java/com/pokedeck/Main.java
@@ -3,34 +3,30 @@ package com.pokedeck;
 import com.pokedeck.cards.*;
 import com.pokedeck.ui.MainWindow;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 public class Main {
 
     public static void main(String[] args) {
-        //Intro
-        System.out.println("==================");
-        System.out.println("==== PokeDeck ====");
-        System.out.println("==================");
-        System.out.println("");
-
-        //Core
+        //Test Cards
         Card card = new EnergyCard("Eau", EnergyType.Water);
         Card card1 = new PokemonCard("Rondoudou", EnergyType.Fairy, 50);
         Card card2 = new TrainerCard("Pokeball", TrainerType.Item);
-
-        System.out.println(card.getCardName());
-        System.out.println(card.getType());
-        System.out.println("");
-        System.out.println(card1.getCardName());
-        System.out.println(card1.getType());
-        System.out.println("");
-        System.out.println(card2.getCardName());
-        System.out.println(card2.getType());
+        Object [] cards = {card.getCardName(), card.getType()};
 
         MainWindow mainWindow = new MainWindow();
         mainWindow.pack();
-        mainWindow.setLocationRelativeTo(null);
-        mainWindow.setTitle("PokeDeck");
         mainWindow.setSize(600,450);
         mainWindow.setVisible(true);
+
+        DefaultTableModel defaultTableModel = new DefaultTableModel();
+        Object [] entetes = {"Name", "Type"};
+        defaultTableModel.addColumn("Name");
+        defaultTableModel.addColumn("Type");
+        defaultTableModel.setColumnIdentifiers(entetes);
+        defaultTableModel.addRow(cards);
+
+        mainWindow.getTable1().setModel(defaultTableModel);
     }
 }
--- src/main/java/com/pokedeck/ui/MainWindow.java
+++ src/main/java/com/pokedeck/ui/MainWindow.java
@@ -1,33 +1,80 @@
 package com.pokedeck.ui;
 
+import com.pokedeck.cards.*;
+
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 public class MainWindow extends JFrame{
-    private JButton addCardButton;
+
     private JPanel panel1;
+    private JButton addCardButton;
     private JButton deleteCardButton;
-    private JList list1;
+    private JList cardList;
     private JTable table1;
 
     public MainWindow() {
+
         this.setContentPane(panel1);
+        this.setTitle("PokeDeck");
+        this.setLocationRelativeTo(null);
+
+        Card card = new EnergyCard("Eau", EnergyType.Water);
+        Card card1 = new PokemonCard("Rondoudou", EnergyType.Fairy, 50);
+        final Card card2 = new TrainerCard("Pokeball", TrainerType.Item);
+
+        final DefaultListModel<String> defaultListModel = new DefaultListModel<String>();
+        defaultListModel.add(0, card.getCardName());
+        defaultListModel.add(1, card1.getCardName());
+        defaultListModel.add(2, card2.getCardName());
+        cardList.setModel(defaultListModel);
+
 
         addCardButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,"Hello");
+                defaultListModel.add(defaultListModel.getSize(),card2.getCardName());
             }
         });
 
-        // call onCancel() when cross is clicked
+        deleteCardButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    defaultListModel.remove(cardList.getSelectedIndex());
+                    cardList.setModel(defaultListModel);
+                } catch (ArrayIndexOutOfBoundsException e1) {
+                    JOptionPane.showMessageDialog(null,"Vous n'avez selectionnez aucune carte");
+                }
+            }
+        });
+
+        cardList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                System.out.print(cardList.getSelectedIndex());
+            }
+        });
+
+
+
+
+
+        // Stop program when close window
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 dispose();
             }
         });
+
+
+    }
+
+    public JList getCardList() {
+        return cardList;
+    }
+
+    public JTable getTable1() {
+        return table1;
     }
 }
