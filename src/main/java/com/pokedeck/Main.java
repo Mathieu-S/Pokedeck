60a49d0e1c7676a70d432aa9a1b9f9d0c37ddcb3
Mathieu-S <mathieusartorio@gmail.com>
2017-03-12 17:26:40 +0100

    Supression code inutile

--- src/main/java/com/pokedeck/Main.java
+++ src/main/java/com/pokedeck/Main.java
@@ -3,9 +3,6 @@ package com.pokedeck;
 import com.pokedeck.cards.*;
 import com.pokedeck.ui.MainWindow;
 
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -13,20 +10,10 @@ public class Main {
         Card card = new EnergyCard("Eau", EnergyType.Water);
         Card card1 = new PokemonCard("Rondoudou", EnergyType.Fairy, 50);
         Card card2 = new TrainerCard("Pokeball", TrainerType.Item);
-        Object [] cards = {card.getCardName(), card.getType()};
 
         MainWindow mainWindow = new MainWindow();
         mainWindow.pack();
         mainWindow.setSize(600,450);
         mainWindow.setVisible(true);
-
-        DefaultTableModel defaultTableModel = new DefaultTableModel();
-        Object [] entetes = {"Name", "Type"};
-        defaultTableModel.addColumn("Name");
-        defaultTableModel.addColumn("Type");
-        defaultTableModel.setColumnIdentifiers(entetes);
-        defaultTableModel.addRow(cards);
-
-        mainWindow.getTable1().setModel(defaultTableModel);
     }
 }
