4b7579961b86b801326c4eb4411954c9c5359d36
Mathieu-S <mathieusartorio@gmail.com>
2017-03-13 22:06:39 +0100

    Delete code inutile

--- src/main/java/com/pokedeck/Main.java
+++ src/main/java/com/pokedeck/Main.java
@@ -1,16 +1,10 @@
 package com.pokedeck;
 
-import com.pokedeck.cards.*;
 import com.pokedeck.ui.MainWindow;
 
 public class Main {
 
     public static void main(String[] args) {
-        //Test Cards
-        Card card = new EnergyCard("Eau", EnergyType.Water);
-        Card card1 = new PokemonCard("Rondoudou", EnergyType.Fairy, 50);
-        Card card2 = new TrainerCard("Pokeball", TrainerType.Item);
-
         MainWindow mainWindow = new MainWindow();
         mainWindow.pack();
         mainWindow.setSize(600,450);
